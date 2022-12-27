725e47385a4e10e86d90246546f27c155360634c
Matus Barta <matus.barta64@gmail.com>
2022-12-27 19:45:02 +0000

    add empty pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <title>
+    InProgress
+  </title>
+</svelte:head>
+
+<p>redirect to login or dashboard if logged in</p>
