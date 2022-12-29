d6364de607001ada0369b7ec03c39512442b0c09
Matus Barta <matus.barta64@gmail.com>
2022-12-29 18:23:54 +0000

    rm import

--- src/routes/api/device/+server.ts
+++ src/routes/api/device/+server.ts
@@ -1,6 +1,5 @@
 import log from '$lib/server/utils/logger';
 import type { RequestHandler } from '../$types';
-import { json } from '@sveltejs/kit';
 import { updateDevicesSchema, type UpdateDevicesSchema } from '$lib/schemas/device.schema';
 import { updateDevice } from '$lib/server/handlers/device.handler';
 
