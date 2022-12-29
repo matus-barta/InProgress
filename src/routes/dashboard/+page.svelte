2524d09a25a5410b45dfc63a06972ba4694c2942
Matus Barta <matus.barta64@gmail.com>
2022-12-29 22:21:22 +0000

    dynamic options for tabs

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
     import Tab from "$lib/components/tab.svelte"
+    import { options } from "$lib/Options";
 
     let unique = {};
 
@@ -14,8 +15,8 @@
 
 <div class="w-fit grid grid-cols-1 gap-5 xl:gap-10 md:grid-cols-2 lg:grid-cols-3">
     {#key unique}
-        <Tab name="InQueue" on:reloadData={reloadData}/>
-        <Tab name="InProgress" on:reloadData={reloadData}/>
-        <Tab name="Done" on:reloadData={reloadData}/>
+        {#each options as option}
+            <Tab name={option} on:reloadData={reloadData}/>
+        {/each}
     {/key}
 </div>
