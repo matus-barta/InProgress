1e0407ee0d4de46b5e7cec5e92b4731cf4db0160
Matus Barta <matus.barta64@gmail.com>
2023-01-01 21:49:36 +0000

    troubleshooting prisma "shouldnt" need changes but too much pain to go back

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
+export default defineConfig(() => {
+  	return {
+		plugins: [sveltekit()],
+		ssr: {
+			external: ["@prisma/client"],
+		},
+  	};
+});
+
+/*
+/** @type {import('vite').UserConfig} *//*
 const config = {
 	plugins: [sveltekit()]
 };
 
 export default config;
+*/
