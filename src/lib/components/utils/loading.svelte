a24b2b3724bd0c4579ef8f245863d85b6f194a63
Matus Barta <matus.barta64@gmail.com>
2023-01-09 22:06:20 +0000

    make it more universal

---
+++ src/lib/components/utils/loading.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Circle } from 'svelte-loading-spinners';
+	export let size = 60;
+	export let padding = true;
+</script>
+
+{#if padding}
+	<div class="w-full flex items-center justify-center">
+		<Circle {size} color="#f1691c" unit="px" duration="1s" />
+	</div>
+{:else}
+	<div class="w-full flex items-center justify-center">
+		<Circle {size} color="#f1691c" unit="px" duration="1s" />
+	</div>
+{/if}
---
+++ src/lib/components/utils/virtualList.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let items: any;
+</script>
+
+{#key items}
+	{#each items as item}
+		<slot {item} />
+	{/each}
+{/key}
