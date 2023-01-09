02a8e8d025c741c677835ea71eb5ea13052ddd42
Matus Barta <matus.barta64@gmail.com>
2023-01-09 23:15:36 +0000

    add error handling

--- src/lib/components/scanner.svelte
+++ src/lib/components/scanner.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { redirect } from '@sveltejs/kit';
 	import { Html5Qrcode } from 'html5-qrcode';
 	import { onMount } from 'svelte';
 	import { createEventDispatcher } from 'svelte';
@@ -8,26 +9,34 @@
 	const submit = () => dispatch('submit');
 
 	let scanning = false;
+	let errorMessage = '';
 
 	let html5Qrcode: Html5Qrcode;
 
 	onMount(init);
 
 	function init() {
+		errorMessage = '';
 		html5Qrcode = new Html5Qrcode('reader');
 	}
 
 	function start() {
-		html5Qrcode.start(
-			{ facingMode: 'environment' },
-			{
-				fps: 10,
-				qrbox: { width: 350, height: 80 },
-				aspectRatio: 1.0
-			},
-			onScanSuccess,
-			onScanFailure
-		);
+		html5Qrcode
+			.start(
+				{ facingMode: 'environment' },
+				{
+					fps: 10,
+					qrbox: { width: 350, height: 80 },
+					aspectRatio: 1.0
+				},
+				onScanSuccess,
+				onScanFailure
+			)
+			.catch((e) => {
+				console.log(e);
+				errorMessage = 'No camera found!';
+				scanning = false;
+			});
 		scanning = true;
 	}
 
@@ -49,6 +58,9 @@
 </script>
 
 <div class="flex flex-col w-full items-center gap-5">
+	{#if errorMessage != ''}
+		<h2 class="font-bold text-red-600">{errorMessage}</h2>
+	{/if}
 	<reader id="reader" class="w-full max-w-xl bg-black" />
 	{#if scanning}
 		<button class="max-w-sm" on:click={stop}>stop</button>
